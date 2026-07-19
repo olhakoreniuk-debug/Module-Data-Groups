9974a0eaef0b5b25b8570245be52b2dc19242647
olhakoreniuk-debug <olha.koreniuk@gmail.com>
2026-07-19 18:03:23 +0200

    Implement image carousel slideshow

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,64 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
+const image = document.querySelector("#carousel-img");
+const backwardButton = document.querySelector("#backward-btn");
+const forwardButton = document.querySelector("#forward-btn");
+const autoBackwardButton = document.querySelector("#auto-backward");
+const autoForwardButton = document.querySelector("#auto-forward");
+const stopButton = document.querySelector("#stop");
 
-// Write your code here
+const interval = 2000;
+let currentImageIndex = 0;
+let slideshowTimer = null;
+
+function showImage() {
+  image.src = images[currentImageIndex];
+}
+
+function moveForward() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  showImage();
+}
+
+function moveBackward() {
+  currentImageIndex =
+    (currentImageIndex - 1 + images.length) % images.length;
+  showImage();
+}
+
+function setAutoButtonsDisabled(disabled) {
+  autoForwardButton.disabled = disabled;
+  autoBackwardButton.disabled = disabled;
+}
+
+function startSlideshow(direction) {
+  if (slideshowTimer !== null) {
+    return;
+  }
+
+  setAutoButtonsDisabled(true);
+  slideshowTimer = window.setInterval(direction, interval);
+}
+
+function stopSlideshow() {
+  if (slideshowTimer !== null) {
+    window.clearInterval(slideshowTimer);
+    slideshowTimer = null;
+  }
+
+  setAutoButtonsDisabled(false);
+}
+
+backwardButton.addEventListener("click", moveBackward);
+forwardButton.addEventListener("click", moveForward);
+autoBackwardButton.addEventListener("click", () =>
+  startSlideshow(moveBackward)
+);
+autoForwardButton.addEventListener("click", () =>
+  startSlideshow(moveForward)
+);
+stopButton.addEventListener("click", stopSlideshow);
